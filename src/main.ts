9bd2735c49837fc45356895df8cb96f83109f31c
Abhijit47 <akarmakar846@gmail.com>
2026-01-01 23:00:50 +0530

    refactor: update start script and improve port handling in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+console.log(
+  `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
+);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -14,7 +19,7 @@ async function bootstrap() {
   //   }),
   // );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap()
   .then(() => {
